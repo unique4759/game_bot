041f71dabdf48c707106e7fd03e57e833f5fac76
Max Kuznetsov <maxkuznec12@gmail.com>
2020-04-05 15:24:41 +0300

    Добавил усложненку

--- script/script.js
+++ script/script.js
@@ -12,34 +12,62 @@ function isNum(n) {
 /**
  * Process game.
  */
-function startGame() {
+function startGame(num, newGame = false) {
     let myNumber = Math.round(Math.random() * 100) + 1;
+
+    if(newGame) {
+        myNumber = Math.round(Math.random() * 100) + 1;
+    }
     console.log(myNumber);
+
     let userNumber = prompt('Угадай число от 0 до 100');
+    let numberAttempts = num;
 
     if(userNumber !== null) {
         function processGame() {
             if(isNum(userNumber)) {
                 if(userNumber > myNumber) {
-                    alert('Загаданное число меньше');
-                    startGame();
+                    numberAttempts--;
+
+                    if(!numberAttempts) {
+                        replayGame();
+                    } else {
+                        alert('Загаданное число меньше, осталось попыток: ' + numberAttempts);
+                        startGame(numberAttempts);
+                    }
                 }
                 else if(userNumber < myNumber) {
-                    alert('Загаданное число больше');
-                    startGame();
+                    numberAttempts--;
+
+                    if(!numberAttempts) {
+                        replayGame();
+                    } else {
+                        alert('Загаданное число больше, осталось попыток: ' + numberAttempts);
+                        startGame(numberAttempts);
+                    }
                 }
                 else {
-                    alert('Угадал');
-                    return;
+                    let replayGame = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+
+                    if(replayGame) {
+                        startGame(10, true);
+                    }
+                    else {
+                        alert('До скорого!');
+                        return;
+                    }
                 }
             }
             else {
                 let validate = confirm('Введи число!');
 
                 if(validate) {
-                    startGame();
+                    numberAttempts--;
+
+                    startGame(numberAttempts);
                 }
                 else {
+                    alert('До скорого!');
                     return;
                 }
             } 
@@ -47,7 +75,22 @@ function startGame() {
         return processGame(); 
     }
     else {
+        alert('До скорого!');
         return;
     }
 }
-startGame();
+startGame(10, true);
+
+/**
+ * Check replay game.
+ */
+function replayGame() {
+    let replay = confirm('Попытки закончились, хотите сыграть еще?');
+
+    if(replay) {
+        startGame(10, true);
+    } 
+    
+    alert('До скорого!');
+    return;
+}
